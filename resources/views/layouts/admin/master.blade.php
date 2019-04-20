ccc6e0567762bcd76dc00b0b38a1809d19a34095
kjayru <wiltinoco@gmail.com>
2019-04-20 10:38:00 -0500

    fix

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>{{ env('APP_NAME')}}</title>
- 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
  @include('layouts.admin.partials.styles')
   
